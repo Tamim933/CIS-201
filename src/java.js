2be7412cab5cbcc865aa28d7fd37cd377d42ba91
Tamim933 <157344324+Tamim933@users.noreply.github.com>
2024-05-08 15:15:25 +0300

    Add files via upload

---
+++ src/java.js
@@ -0,0 +1,28 @@
+
+
+let food1s = document.getElementById('food1');
+let food2s = document.getElementById('food2');
+let food3s = document.getElementById('food3');
+let food4s = document.getElementById('food4');
+let food5s = document.getElementById('food5');
+let food6s = document.getElementById('food6');
+let foods = document.getElementById('food');
+
+food1s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url(''Photo/photo2.png'')";
+})
+food2s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('Photo/photo3.png')";
+})
+food3s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('Photo/photo4.png')";
+})
+food4s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('Photo/photo5.png')";
+})
+food5s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('Photo/photo6.png')";
+})
+food6s.addEventListener('click',()=>{
+    foods.style.backgroundImage="url('Photo/photo7.png')";
+})
